1206cfe4749c3099544be50ba1520a9eb011af82
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-04-01 16:21:15 -0400

    Increase the range of input string length from 8-15 to 8-32

--- driver.rb
+++ driver.rb
@@ -16,7 +16,7 @@ def main
   best_data = nil
   randall_data = "5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6"
 
-  while (n = Array.new(rand(16)+8){range.sample}.join) do
+  while (n = Array.new(rand(25)+8){range.sample}.join) do
     skein = SkeinR::Hash1024.new(1024)
     skein.update_str n
     res = SkeinR::bytes_to_hex(skein.final).downcase
